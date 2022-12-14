39168f9c97016a57a976773a90424bd524551f12
pzerok <lolanse2008@gmail.com>
2022-12-14 18:42:15 +0900

    branchTest

--- Test3/src/main/java/com/pdf/controller/PdfController.java
+++ Test3/src/main/java/com/pdf/controller/PdfController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.pdf.service.IPdfService;
 
 /*
  * 2.1.7 은 Laowagie라는 도메인을 갖고 있었던 마지막 버전이다. 일단 테스트용으로 2.1.7로 구성해봤지만,
@@ -37,13 +41,19 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PdfController {
 	
+	@Inject
+	IPdfService service;
+	//서비스 객체를 사용하기 위해 의존성을 주입
+	private static Logger logger = LoggerFactory.getLogger(PdfController.class);
+	
+	
 	@RequestMapping(value = "/download/pdf", method = RequestMethod.GET)
 	public ModelAndView downloadPdf() {
 	    ModelAndView modelAndView = new ModelAndView("pdfDownView", "fileName", "test.pdf");
 	    return modelAndView;
 	}
 	
-	private static Logger logger = LoggerFactory.getLogger(PdfController.class);
+	
 	@RequestMapping("/")
 	public String home() {
 		return "home";
@@ -51,6 +61,7 @@ public class PdfController {
 	
 	// RequestMapping 은 <a> 태그와 일치하면 되며 원하는 출력단을 pdf로서 동작시키기 위해선 return 뷰 이름에 pdf가 포함되어야하며 
 	// 이 뷰 리턴이 bean 객체에 포함된다.
+	
 	@RequestMapping(value="test", method=RequestMethod.GET)
 	public String pdfList(Model model){
 		
@@ -85,4 +96,12 @@ public class PdfController {
 		//출력할 뷰 이름 리턴 _ Servlet-context에서 설정한 bean값의 id와 일치하면 AbstractPdfView가 동작하여 pdf문서를 작성한다.
 		return "pdf";
 	}
+	
+	  
+	    
+	    @RequestMapping("/pdf/list.do") //View에서 맵핑 url 주소
+	    public ModelAndView list(int reNo) throws Exception {
+	        String result = service.createPdf(reNo); //createPdf()메소드에서 pdf파일이 생성되었는지 결과가 result에 담긴다.
+	        return new ModelAndView("pdf/result","message",result); //그 결과가 message로 pdf/result페이지로 전송된다.
+	    }
 }
